40ade366560507e0e640cdf0e5cb09f7c2a77d4b
Mark Lee <electronjs@lazymalevolence.com>
2021-08-24 00:39:30 -0700

    fix(typescript): strict is implied

--- src/typescript.js
+++ src/typescript.js
@@ -1,6 +1,11 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "eslint-plugin-tsdoc"],
+  parserOptions: {
+    ecmaFeatures: {
+      impliedStrict: true,
+    },
+  },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
