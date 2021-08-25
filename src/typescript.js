7f19169a1401bd134a10fcbdb612bfae2f72e7bb
Mark Lee <electronjs@lazymalevolence.com>
2021-08-25 07:55:39 -0700

    refactor(typescript): only extend TypeScript-specific rules

--- src/typescript.js
+++ src/typescript.js
@@ -7,15 +7,8 @@ module.exports = {
     },
   },
   extends: [
-    "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:ava/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
     "plugin:import/typescript",
-    "plugin:prettier/recommended",
-    "plugin:promise/recommended",
-    "prettier",
   ],
   rules: {
     // There is no "recommended" ruleset for tsdoc yet.
