dc3ed565838b8efc023cb8f0909844a8e857d994
rtritto <ariele.1994@gmail.com>
2024-09-20 01:19:48 +0200

    chore: use ava as optional peer dependency

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const _extends = ["./src/common"];
+
+try {
+  require.resolve("eslint-plugin-ava");
+  _extends.push("./src/ava");
+} catch {}
+
 module.exports = {
-  extends: ["./src/common", "./src/ava"],
+  extends: _extends,
 };
